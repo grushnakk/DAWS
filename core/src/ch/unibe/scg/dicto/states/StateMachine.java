d98f787ea5b7af6574efeb3ecb2e631f86572f52
Kenneth Radunz <kenneth.radunz@gmail.com>
2015-03-25 20:56:38 +0100

    states are now lazy referenced inside the state_machine via an ID.

--- core/src/ch/unibe/scg/dicto/states/StateMachine.java
+++ core/src/ch/unibe/scg/dicto/states/StateMachine.java
@@ -1,21 +1,38 @@
 package ch.unibe.scg.dicto.states;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ch.unibe.scg.dicto.parser.Context;
 
 public class StateMachine {
 
 	private State active;
+	private Map<Integer, State> stateMap;
 	private final Context context;
 	private final Environment environment;
 	
-	public StateMachine(State start, Context context, Environment environment) {
-		this.active = start;
+	public StateMachine(Context context, Environment environment, int activeStateID, Map<Integer, State> stateMap) {
+		this.stateMap = new HashMap<Integer, State>(stateMap);
 		this.context = context;
 		this.environment = environment;
+		setStateToActive(activeStateID);
 	}
 	
-	public void setActiveState(State state) {
-		this.active = state;
+	public StateMachine(Context context, Environment environment, int activeStateID) {
+		this(context, environment, activeStateID, new HashMap<Integer, State>());
+	}
+	
+	public void setStateToActive(int stateID) { //TODO long name
+		this.active = stateMap.get(stateID);
+	}
+	
+	public void addState(int stateID, State state) {
+		stateMap.put(stateID, state);
+	}
+	
+	public boolean hasState(int stateID) {
+		return stateMap.containsKey(stateID);
 	}
 	
 	public Context getContext() {
