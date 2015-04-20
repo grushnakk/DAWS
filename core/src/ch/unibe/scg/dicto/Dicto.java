3cde04581522398982073f55f5edbad23d27ec93
Kenneth Radunz <kenneth.radunz@gmail.com>
2015-04-20 11:46:28 +0200

    new tests

--- core/src/ch/unibe/scg/dicto/Dicto.java
+++ core/src/ch/unibe/scg/dicto/Dicto.java
@@ -21,9 +21,12 @@ public class Dicto {
 	
 	private static final int ID_START 				= 00;
 	private static final int ID_TYPE 				= 01;
-	private static final int ID_WITH_KEYWORD 		= 02;
-	private static final int ID_RULE 				= 10;
-	private static final int ID_KNOW_ID_AFTER_ONLY 	= 20;
+	private static final int ID_KEYWORD_WITH 		= 02;
+	private static final int ID_ARG_NAME			= 03;
+	private static final int ID_ARG_ASSIGN			= 04;
+	private static final int ID_ARG_VALUE			= 05;
+//	private static final int ID_RULE 				= 10;
+//	private static final int ID_KNOW_ID_AFTER_ONLY 	= 20;
 	
 	private static final String REGION_IDENTIFIER = "ID";
 	
@@ -31,6 +34,10 @@ public class Dicto {
 	private static Acceptor IDENTIFIER_ACCEPTOR;
 	private static Path NEW_ID_PATH;
 	private static Path TYPE_PATH;
+	private static Path WITH_PATH;
+	private static Path ARG_NAME_PATH;
+	private static Path ARG_ASSIGN_PATH;
+	private static Path ARG_STRING_PATH;
 	
 	static {
 		buildParts();
@@ -38,6 +45,9 @@ public class Dicto {
 		DICTO_MACHINE = new StateMachine(ID_START);
 		DICTO_MACHINE.addState(ID_START, new State(NEW_ID_PATH));
 		DICTO_MACHINE.addState(ID_TYPE, new State(TYPE_PATH));
+		DICTO_MACHINE.addState(ID_KEYWORD_WITH, new State(WITH_PATH));
+		DICTO_MACHINE.addState(ID_ARG_NAME, new State(ARG_NAME_PATH));
+		DICTO_MACHINE.addState(ID_ARG_ASSIGN, new State(ARG_ASSIGN_PATH));
 	}
 	
 	static void buildParts() {
@@ -55,13 +65,40 @@ public class Dicto {
 			public StateResult onNext(Environment env, AcceptorResult result) {
 				return new Next(ID_TYPE);
 			}
-		}, NO_SUGGESTIONS);
-		
+		}, NO_SUGGESTIONS);		
 		TYPE_PATH = new Path(IDENTIFIER_ACCEPTOR.chain(optionalWhitespace()), new NextAction() {
 			
 			@Override
 			public StateResult onNext(Environment env, AcceptorResult result) {
-				return new Next(ID_WITH_KEYWORD);
+				return new Next(ID_KEYWORD_WITH);
+			}
+		}, NO_SUGGESTIONS);
+		WITH_PATH = new Path(string("with").chain(optionalWhitespace()), new NextAction() {
+			
+			@Override
+			public StateResult onNext(Environment env, AcceptorResult result) {
+				return new Next(ID_ARG_NAME);
+			}
+		}, NO_SUGGESTIONS);
+		ARG_NAME_PATH = new Path(IDENTIFIER_ACCEPTOR.chain(optionalWhitespace()), new NextAction() {
+			
+			@Override
+			public StateResult onNext(Environment env, AcceptorResult result) {
+				return new Next(ID_ARG_ASSIGN);
+			}
+		}, NO_SUGGESTIONS);
+		ARG_ASSIGN_PATH = new Path(string(":").chain(optionalWhitespace()), new NextAction() {
+			
+			@Override
+			public StateResult onNext(Environment env, AcceptorResult result) {
+				return new Next(ID_ARG_VALUE);
+			}
+		}, NO_SUGGESTIONS);
+		ARG_NAME_PATH = new Path(string("\"").chain(IDENTIFIER_ACCEPTOR, string("\""), optionalWhitespace()), new NextAction() {
+			
+			@Override
+			public StateResult onNext(Environment env, AcceptorResult result) {
+				return new Next(ID_START);
 			}
 		}, NO_SUGGESTIONS);
 	}
